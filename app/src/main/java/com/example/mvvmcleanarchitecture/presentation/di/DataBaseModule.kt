97073c883166cc45aff70342b357342fa32ba2b7
yunjaem06 <yunjae0616@gmail.com>
2026-02-18 14:24:43 +0900

    모듈, DataBase

---
+++ app/src/main/java/com/example/mvvmcleanarchitecture/presentation/di/DataBaseModule.kt
@@ -0,0 +1,38 @@
+package com.example.mvvmcleanarchitecture.presentation.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.mvvmcleanarchitecture.data.db.ArtistDao
+import com.example.mvvmcleanarchitecture.data.db.MovieDao
+import com.example.mvvmcleanarchitecture.data.db.TMDBDatabase
+import com.example.mvvmcleanarchitecture.data.db.TvShowDao
+import dagger.Provides
+import javax.inject.Singleton
+
+class DataBaseModule {
+
+    @Singleton
+    @Provides
+    fun provideMovieDataBase(context: Context): TMDBDatabase {
+        return Room.databaseBuilder(context,TMDBDatabase::class.java,"tmdbclient")
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMovieDao(tmdbDatabase: TMDBDatabase):MovieDao{
+        return tmdbDatabase.movieDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvDao(tmdbDatabase: TMDBDatabase): TvShowDao {
+        return tmdbDatabase.tvDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistDao(tmdbDatabase: TMDBDatabase): ArtistDao {
+        return tmdbDatabase.artistDao()
+    }
+}
