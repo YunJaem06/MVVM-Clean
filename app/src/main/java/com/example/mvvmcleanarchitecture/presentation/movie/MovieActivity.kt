8e6b1796c9bb09e165786eb0870b2573433ebf50
yunjaem06 <yunjae0616@gmail.com>
2026-04-18 12:33:16 +0900

    [feat] Hilt, ViewModel 연결

--- app/src/main/java/com/example/mvvmcleanarchitecture/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/mvvmcleanarchitecture/presentation/movie/MovieActivity.kt
@@ -1,20 +1,33 @@
 package com.example.mvvmcleanarchitecture.presentation.movie
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmcleanarchitecture.R
 import com.example.mvvmcleanarchitecture.databinding.ActivityMovieBinding
+import com.example.mvvmcleanarchitecture.presentation.di.Injector
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var factory: MovieViewModelFactory
+    private lateinit var movieViewModel: MovieVIewModel
     private lateinit var binding : ActivityMovieBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
+        (application as Injector).createMovieSubComponent()
+            .inject(this)
+
+        movieViewModel = ViewModelProvider(this, factory)[MovieVIewModel::class.java]
+        val responseLiveData = movieViewModel.getMovies()
+        responseLiveData.observe(this, Observer {
+            Log.i("MYTAG",it.toString())
+        })
     }
 }
